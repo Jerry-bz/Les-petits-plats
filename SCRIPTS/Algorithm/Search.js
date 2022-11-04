dc0d6cf43fca65059dc436675101d840513aebfa
unknown <jerry.brzt@gmail.com>
2022-11-04 15:56:19 +0100

    algorithme avec la méthode foreach

--- SCRIPTS/Algorithm/Search.js
+++ SCRIPTS/Algorithm/Search.js
@@ -4,37 +4,32 @@ import recipeFactory from '../Factories/factoryRecipe.js';
 import searchButtonClick from '../Utils/displayTag.js';
 import { displayNone, displayMessage } from '../Utils/displayMessage.js';
 
+
 // Recherche Principale qui affiche les recettes 
 const searchNavInputRecipe = (inputTextValue, allRecipes) => {
+
 	let arrayResultSearch = [];
 	if (inputTextValue.length < 3) {
 		arrayResultSearch = allRecipes;
 	}
+
 	// Recherche active à partir de 3 caractères dans la barre de recherche principale
 	if (inputTextValue.length >= 3) {
 		arrayResultSearch = [];
 
-		for (let recipe of allRecipes) {
-			// Tableau qui récupère la saise du nom et de la decription en lien avec les données
-			if (
-				recipe.name.toLowerCase().includes(inputTextValue.toLowerCase()) ||
-        recipe.description.toLowerCase().includes(inputTextValue.toLowerCase())
-			) {
+		allRecipes.forEach((recipe) => {
+			if (recipe.name.toLowerCase().includes(inputTextValue.toLowerCase()) || recipe.description.toLowerCase().includes(inputTextValue.toLowerCase())) {
 				arrayResultSearch.push(recipe);
 			} else {
-				// Tableau qui récupère la saise des ingrédients en lien avec les données
-				for (let element of recipe.ingredients) {
-					if (
-						element.ingredient
-							.toLowerCase()
-							.includes(inputTextValue.toLowerCase())
-					) {
+				recipe.ingredients.forEach((element) => {
+					if (element.ingredient.toLowerCase().includes(inputTextValue.toLowerCase())) {
 						// Retourne le resultat de la saisie 
 						arrayResultSearch.push(recipe);
 					}
-				}
+				});
 			}
-		}
+		});
+
 		// Message "aucune recette";
 		arrayResultSearch.length === 0 ? displayMessage() : displayNone();
 	}
@@ -46,14 +41,12 @@ const searchNavInputRecipe = (inputTextValue, allRecipes) => {
 // Recherche de la saisie des Filtres (retourne un tableau des résultats de la saisie)
 const searchButtonsInput = (inputValue, array) => {
 	let arrayResult = [];
-	if (inputValue.length >= 0) {
-		for (let element of array) {
-			if (element.toLowerCase().includes(inputValue.toLowerCase())) {
-				arrayResult.push(element);
-			}
-		}
-		return arrayResult;
-	}
+
+	inputValue.length >= 0 && array.forEach(element => {
+		element.toLowerCase().includes(inputValue.toLowerCase()) && arrayResult.push(element);
+	});
+
+	return arrayResult;
 };
 
 
@@ -69,50 +62,39 @@ const filterTag = (recipes) => {
 
 	if (tags.length != 0) {
 
-		for (let tag of tags) {
+		tags.forEach(tag => {
 			tag.className.includes('ingredient') && listTagIngredient.push(tag.textContent.toLowerCase());
 			tag.className.includes('device') && listTagDevices.push(tag.textContent.toLowerCase());
 			tag.className.includes('utensil') && listTagUtensils.push(tag.textContent.toLowerCase());
-		}
+		});
 
-		for (let recipe of recipes) {
+		recipes.forEach((recipe) => {
 			/*   filtre par ingredient    */
 			let exist = 0;
-			for (let ingredient of recipe.ingredients) {
-				listTagIngredient.includes(ingredient.ingredient.toLowerCase()) && exist++;
-			}
+			recipe.ingredients.forEach((element) => {
+				listTagIngredient.includes(element.ingredient.toLowerCase()) && exist++;
+			});
 			exist === listTagIngredient.length && listbyingredient.push(recipe);
 
 			/*   filtre par device   */
-			if (listTagDevices.length === 0) {
-				listbydevice = recipes;
-			} else {
-				listTagDevices.includes(recipe.appliance.toLowerCase()) &&
-          listbydevice.push(recipe);
-			}
+			listTagDevices.length === 0 ? listbydevice = recipes : listbydevice = recipes;
 
 			/*   filtre par ustensile   */
 			let existUst = 0;
-			for (let ust of recipe.ustensils) {
+			recipe.ustensils.forEach((ust) => {
 				listTagUtensils.includes(ust.toLowerCase()) && existUst++;
-			}
+			});
 			existUst === listTagUtensils.length && listbyustensil.push(recipe);
-		}
-
+		});
 	}
 
 	let result = [];
 
-	for (let recipe of recipes) {
-		if (
-			listbydevice.includes(recipe) &&
-      listbyustensil.includes(recipe) &&
-      listbyingredient.includes(recipe)
-		) {
-			result.push(recipe);
-		}
-	}
+	recipes.forEach((recipe) => {
+		listbydevice.includes(recipe) && listbyustensil.includes(recipe) && listbyingredient.includes(recipe) && result.push(recipe);
+	});
 
+	// Si tableau vide on affiche toutes les recettes
 	if (result.length === 0) {
 		result = recipes;
 	}
@@ -142,9 +124,9 @@ const filterTag = (recipes) => {
 	displayListUtensils(listOfUtensils);
 	sectionRecipes.innerHTML = '';
 
-	for (let recipe of result) {
+	result.forEach(recipe => {
 		sectionRecipes.appendChild(recipeFactory(recipe));
-	}
+	});
 
 	searchButtonClick();
 };
